edceb4fef35f5b3bf3764f856285b00df110fec8
Sharlene <sharlene.liao@timecitytech.com>
2018-07-17 17:53:56 +0800

    201/0717 更新顯示順序

--- js/data.js
+++ js/data.js
@@ -34,6 +34,21 @@ var vm = new Vue({
             {
                 newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
                 importanttag: true, //产品专栏活动专栏头版,内容发布重点标签
+                istopbanner: true, //是否首页展示
+                clickto: false, //是否有内页跳窗公告
+                link: "action/20180605_pngpage/index.html", //点击连结,下载檔案
+                downloadbtn: true, //是否有下载按钮
+                downloadlink: "loading/20180607_XIN.rar", //下载连结
+                mainbannerimg: "images/activebanner/20180605_png.jpg", //首页大图2500*1080
+                bannerimg: "images/activebanner/20180605_pngs.jpg", //公告内页小图1410*385
+                name: "PLAY'n GO 独特灵感和创造力!!", //公告名称(最好8个全形字以内),download活动素材/轮播图
+                time: "2018/06", //活动日期
+                containtexts: '',
+                containtext: '' //内容html
+            },
+            {
+                newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
+                importanttag: false, //产品专栏活动专栏头版,内容发布重点标签
                 istopbanner: false, //是否首页展示
                 clickto: false, //是否有内页跳窗公告
                 link: "action/20180717_news/index.html", //点击连结,下载檔案
@@ -93,21 +108,6 @@ var vm = new Vue({
                 containtext: ''
             },
             */
-            {
-                newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
-                importanttag: false, //产品专栏活动专栏头版,内容发布重点标签
-                istopbanner: true, //是否首页展示
-                clickto: false, //是否有内页跳窗公告
-                link: "action/20180605_pngpage/index.html", //点击连结,下载檔案
-                downloadbtn: true, //是否有下载按钮
-                downloadlink: "loading/20180607_XIN.rar", //下载连结
-                mainbannerimg: "images/activebanner/20180605_png.jpg", //首页大图2500*1080
-                bannerimg: "images/activebanner/20180605_pngs.jpg", //公告内页小图1410*385
-                name: "PLAY'n GO 独特灵感和创造力!!", //公告名称(最好8个全形字以内),download活动素材/轮播图
-                time: "2018/06", //活动日期
-                containtexts: '',
-                containtext: '' //内容html
-            },
             {
                 newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
                 importanttag: false, //产品专栏活动专栏头版,内容发布重点标签
