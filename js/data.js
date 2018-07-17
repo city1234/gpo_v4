78b196793cd24a85f7afefad94d60380494deee7
Sharlene <sharlene.liao@timecitytech.com>
2018-07-17 17:50:54 +0800

    data.js upgraded

--- js/data.js
+++ js/data.js
@@ -36,7 +36,7 @@ var vm = new Vue({
                 importanttag: true, //产品专栏活动专栏头版,内容发布重点标签
                 istopbanner: false, //是否首页展示
                 clickto: false, //是否有内页跳窗公告
-                link: "/action/20180717_news/index.html", //点击连结,下载檔案
+                link: "action/20180717_news/index.html", //点击连结,下载檔案
                 downloadbtn: true, //是否有下载按钮
                 downloadlink: "loading/20180717_news.zip", //下载连结
                 mainbannerimg: "images/activebanner/20180717_news.jpg", //首页大图2500*1080
