7affc6b6236bc94748728a40267b865cc9cb51c6
Sharlene <sharlene.liao@timecitytech.com>
2018-07-20 13:08:54 +0800

    20180720 表格更新

--- js/data.js
+++ js/data.js
@@ -257,7 +257,7 @@ var vm = new Vue({
                             1.	接入<a href="action/20180605_pngpage/index.html" target="_blank">PNG电子</a><br />\
                             2.	接入<a href="action/20180522_bggame/index.html" target="_blank">BG平台</a>，包括：BG视讯、BG捕鱼、BG电子<br />\
                             3.	AG拆分位AG电子和<a href="http://yoplay.com/index.php#yo80s" target="_blank">YOPALY电子</a>，增设各个电子游戏入口<br />\
-                            4.	OG体育\
+                            4.	<a href="action/20180622_og_intro/index.html" target="_blank">OG体育</a>\
                         </td>\
                     </tr>\
                     <tr>\
