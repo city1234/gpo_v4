da06a447b563cc0f304465d58df059fecc8b646a
a08864402 <jacky.chen@timecitytech.com>
2018-08-23 13:53:54 +0800

    updata

--- js/data.js
+++ js/data.js
@@ -33,7 +33,7 @@ var vm = new Vue({
         newss: [
             {
                 newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
-                importanttag: false, //产品专栏活动专栏头版,内容发布重点标签
+                importanttag: true, //产品专栏活动专栏头版,内容发布重点标签
                 istopbanner: true, //是否首页展示
                 clickto: false, //是否有内页跳窗公告
                 link: "https://elegant-wescoff-78d55a.netlify.com/", //点击连结,下载檔案
@@ -48,7 +48,7 @@ var vm = new Vue({
             },
             {
                 newstype: 0, //资讯子分页类型 product>0,content>1,event>2,download>3
-                importanttag: true, //产品专栏活动专栏头版,内容发布重点标签
+                importanttag: false, //产品专栏活动专栏头版,内容发布重点标签
                 istopbanner: true, //是否首页展示
                 clickto: false, //是否有内页跳窗公告
                 link: "action/20180605_pngpage/index.html", //点击连结,下载檔案
