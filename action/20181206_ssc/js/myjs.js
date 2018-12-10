1fbd63b7cf2b14024449c35b9695a2a12a85ff81
a08864402 <jacky.chen@timecitytech.com>
2018-12-10 15:08:11 +0800

    ssc modfi

--- action/20181206_ssc/js/myjs.js
+++ action/20181206_ssc/js/myjs.js
@@ -52,4 +52,12 @@ $(document).ready(function() {
         $("#bgvid").remove();
     } else {
     };
+    $(function() {
+        $('a.scroll[href*="#"]').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 500, 'linear');
+        });
+    });
 });
