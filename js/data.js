8934113270728dee01326877de297d2d9f1c3827
a08864402 <jacky.chen@timecitytech.com>
2019-03-11 10:06:37 +0800

    modfi

--- js/data.js
+++ js/data.js
@@ -442,7 +442,7 @@ var vm = new Vue({
       },
       {
         newstype: 1,
-        importanttag: true,
+        importanttag: false,
         istopbanner: false,
         clickto: true,
         link: "javascript: void(0)",
